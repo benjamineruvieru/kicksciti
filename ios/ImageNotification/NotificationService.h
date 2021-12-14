d36294222bfbe19a27fcf3bd89ef40d58a1975e8
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2021-12-14 08:31:15 +0100

    [NotificationService] Add iOS notification service extension
    This commit adds necessary files and configurations to implement a
    notification service extension for iOS. The extension modifies the
    notification content and appends "[modified]" to the title. This allows
    for customizing the appearance of notifications before they are
    delivered to the user.

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by mac on 13/12/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
